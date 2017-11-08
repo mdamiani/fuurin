a4be9a968ab78d3d95371fe8ddb78008028e2d7f
Mirko Damiani <mirko.damiani@outlook.com>
2017-11-08 01:16:53 +0100

    log: test install of custom message handler

--- test/test_common.cpp
+++ test/test_common.cpp
@@ -8,15 +8,21 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include "fuurin/fuurin.h"
-
 #define BOOST_TEST_MODULE common
 #include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+
+#include "fuurin/fuurin.h"
+#include "log.h"
 
 #include <cstdio>
 #include <string>
 
-BOOST_AUTO_TEST_CASE(version) {
+namespace bdata = boost::unit_test::data;
+
+
+BOOST_AUTO_TEST_CASE(version)
+{
     char buf[16];
 
     std::snprintf(buf, sizeof(buf), "%d.%d.%d",
@@ -25,4 +31,35 @@ BOOST_AUTO_TEST_CASE(version) {
         fuurin::VERSION_PATCH);
 
     BOOST_TEST(std::string(fuurin::version()) == std::string(buf));
+}
+
+
+static std::string _myLogOutput;
+static const char * _myLogPrefix[] = {
+    "MY_DEBUG: ",
+    "MY_INFO: ",
+    "MY_WARN: ",
+    "MY_ERROR: ",
+    "MY_FATAL: ",
+};
+
+void myLogHandler(fuurin::LogLevel level, const std::string &message)
+{
+    _myLogOutput = _myLogPrefix[level] + message;
+}
+
+BOOST_DATA_TEST_CASE(customLogMessageHandler, bdata::make({
+    fuurin::DebugLevel,
+    fuurin::InfoLevel,
+    fuurin::WarningLevel,
+    fuurin::ErrorLevel,
+    fuurin::FatalLevel,
+}), level)
+{
+    const std::string msg = "message";
+
+    fuurin::logInstallMessageHandler(myLogHandler);
+    fuurin::logMessage(level, msg);
+
+    BOOST_TEST(_myLogOutput == _myLogPrefix[level] + msg);
 }
