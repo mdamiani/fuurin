ff88f7d2135901adf1f0ba6d0970adddad54040b
Mirko Damiani <mirko.damiani@outlook.com>
2020-08-22 22:42:01 +0200

    lrucache: class implementing a least recently used cache

---
+++ include/fuurin/lrucache.h
@@ -0,0 +1,252 @@
+/**
+ * Copyright (c) Contributors as noted in the AUTHORS file.
+ *
+ * This Source Code Form is part of *fuurin* library.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef FUURIN_LRUCACHE_H
+#define FUURIN_LRUCACHE_H
+
+#include <initializer_list>
+#include <list>
+#include <optional>
+#include <unordered_map>
+#include <utility>
+
+
+namespace fuurin {
+
+/**
+ * \brief Class implementing a Least Recently Used Cache.
+ *
+ * Items are stored in a list, ordered from the least recently used item
+ * (front), to the most recently used one (back).
+ *
+ * The cache has a capacity parameter. When the size of the cache reaches the
+ * capacity limit, then upon insertion of a new item, the least recently used
+ * one will be removed.
+ */
+template<typename K, typename V>
+class LRUCache
+{
+public:
+    ///< List item alias.
+    using Item = std::pair<K, V>;
+    ///< List alias.
+    using List = std::list<Item>;
+
+
+public:
+    /**
+     * \brief Initializes a cache with zero capacity.
+     */
+    LRUCache()
+        : capacity_{0}
+    {
+    }
+
+
+    /**
+     * \brief Constructor.
+     *
+     * Creates a cache object with the passed capacity.
+     *
+     * \param[in] sz Cache capacity.
+     */
+    LRUCache(size_t sz)
+        : capacity_{sz}
+    {
+    }
+
+
+    /**
+     * \brief Constructor with initializer.
+     */
+    LRUCache(const std::initializer_list<Item>& l)
+        : capacity_{l.size()}
+    {
+        for (const auto& v : l)
+            put(v.first, v.second);
+    }
+
+
+    /**
+     * \brief Destructor.
+     */
+    virtual ~LRUCache() noexcept = default;
+
+
+    /**
+     * \return Cache capacity.
+     */
+    size_t capacity() const noexcept
+    {
+        return capacity_;
+    }
+
+
+    /**
+     * \return Cache size.
+     */
+    size_t size() const
+    {
+        return list_.size();
+    }
+
+
+    /**
+     * \return Whether cache size is zero.
+     */
+    bool empty() const
+    {
+        return size() == 0;
+    }
+
+
+    /**
+     * \brief Puts or updates an item into the cache.
+     *
+     * The passed item is set as the most recently used.
+     *
+     * In case the same key is already present in cache,
+     * then its usage is updated and moved to the end
+     * of the list.
+     *
+     * In case the key needs to be added and the capacity
+     * was already reached, then the least recently used
+     * item will be removed.
+     *
+     * \param[in] k Item's key.
+     * \param[in] v Item's value.
+     *
+     * \return An iterator to the new inserted item,
+     *         that is the last element of the \ref list().
+     *         Otherwise \ref list().end() in case of
+     *         zero capacity.
+     */
+    typename List::iterator put(const K& k, const V& v)
+    {
+        if (capacity_ <= 0)
+            return list_.end();
+
+        if (auto it = map_.find(k); it == map_.end()) {
+            if (map_.size() >= capacity_) {
+                const auto& el = list_.front();
+                map_.erase(el.first);
+                list_.pop_front();
+            }
+        } else {
+            list_.erase(it->second);
+        }
+
+        list_.emplace_back(k, v);
+        const auto it = --list_.end();
+        map_[k] = it;
+        return it;
+    }
+
+
+    /**
+     * \brief Gets an item from the cache.
+     *
+     * If the passed key is contained in the cache,
+     * then it is removed and returned.
+     *
+     * \param[in] Item's key.
+     *
+     * \return Optionally the removed item.
+     */
+    std::optional<Item> get(const K& k)
+    {
+        const auto it = map_.find(k);
+
+        if (it == map_.end())
+            return {};
+
+        const Item ret{*it->second};
+        map_.erase(k);
+        list_.erase(it->second);
+
+        return {ret};
+    }
+
+
+    /**
+     * \brief Finds an item by its key.
+     *
+     * The cache list is not modified.
+     *
+     * \param[in] Item's key.
+     *
+     * \return An iterator to the cache list.
+     */
+    ///{@
+    typename List::iterator find(const K& k)
+    {
+        const auto it = map_.find(k);
+        if (it == map_.end())
+            return list_.end();
+
+        return it->second;
+    }
+
+    typename List::const_iterator find(const K& k) const
+    {
+        const auto it = map_.find(k);
+        if (it == map_.end())
+            return list_.end();
+
+        return it->second;
+    }
+    ///@}
+
+
+    /**
+     * \return The cache list.
+     */
+    ///{@
+    List& list()
+    {
+        return list_;
+    }
+
+    const List& list() const
+    {
+        return list_;
+    }
+    ///@}
+
+
+    /**
+     * \brief Comparison operator.
+     *
+     * \param[in] rhs Other cache to compare.
+     *
+     * \return \c true in case the cache list has the same items and the same ordering.
+     */
+    ///{@
+    bool operator==(const LRUCache<K, V>& rhs) const
+    {
+        return list_ == rhs.list_;
+    }
+
+    bool operator!=(const LRUCache<K, V>& rhs) const
+    {
+        return !(*this == rhs);
+    }
+    ///@}
+
+
+private:
+    const size_t capacity_;                              ///< Cache capacity, i.e. max size.
+    List list_;                                          ///< List of cache items.
+    std::unordered_map<K, typename List::iterator> map_; ///< Items position in cache list.
+};
+
+} // namespace fuurin
+
+#endif // FUURIN_LRUCACHE_H
--- test/test_common.cpp
+++ test/test_common.cpp
@@ -15,6 +15,7 @@
 
 #include "fuurin/fuurin.h"
 #include "fuurin/logger.h"
+#include "fuurin/lrucache.h"
 #include "log.h"
 #include "types.h"
 
@@ -23,6 +24,7 @@
 #include <vector>
 #include <tuple>
 #include <functional>
+#include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <utility>
@@ -30,6 +32,7 @@
 #include <string_view>
 #include <errno.h>
 #include <thread>
+#include <ostream>
 
 
 using namespace std::literals;
@@ -465,4 +468,127 @@ BENCHMARK(BM_LogArgArray);
 BOOST_AUTO_TEST_CASE(bench, *utf::disabled())
 {
     ::benchmark::RunSpecifiedBenchmarks();
+}
+
+
+/**
+ * LRUCache
+ */
+using CacheT = LRUCache<std::string, int>;
+
+namespace std {
+std::ostream& operator<<(std::ostream& os, const CacheT::Item& v)
+{
+    os << v.first << ":" << v.second;
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const CacheT::List::iterator& v)
+{
+    os << *v;
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const CacheT::List::const_iterator& v)
+{
+    os << *v;
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const CacheT& c)
+{
+    os << "[";
+    const char* sep = "";
+    for (const auto& v : c.list()) {
+        os << sep;
+        os << v;
+        sep = ", ";
+    }
+    os << "]";
+    return os;
+}
+} // namespace std
+
+
+BOOST_AUTO_TEST_CASE(testLRUCache)
+{
+    // init
+    size_t sz = 3;
+    CacheT c{sz};
+
+    const auto testCache = [&c, sz](size_t len, const CacheT& e) {
+        BOOST_TEST(c.capacity() == sz);
+        BOOST_TEST(c.size() == len);
+        BOOST_TEST(c.empty() == (len == 0));
+        BOOST_TEST(c == e);
+    };
+
+    testCache(0, {});
+
+    // comparison
+    CacheT e1{{"a", 1}};
+    CacheT e2{{"a", 1}, {"b", 2}};
+    CacheT e3{{"a", 1}, {"b", 2}};
+    BOOST_TEST(e1 != e2);
+    BOOST_TEST(e2 == e3);
+
+    // put: add item
+    std::string k = "a";
+    int v = 1;
+    BOOST_TEST((c.put(k, v) != c.list().end()));
+    testCache(1, {{"a", 1}});
+
+    c.put("b", 2);
+    testCache(2, {{"a", 1}, {"b", 2}});
+
+    // put: update item
+    c.put("a", 3);
+    testCache(2, {{"b", 2}, {"a", 3}});
+
+    // put: add item, below capacity
+    c.put("c", 5);
+    testCache(3, {{"b", 2}, {"a", 3}, {"c", 5}});
+
+    // put: update item, below capacity
+    c.put("b", 3);
+    testCache(3, {{"a", 3}, {"c", 5}, {"b", 3}});
+
+    // put: add item, over capacity
+    c.put("d", 6);
+    testCache(3, {{"c", 5}, {"b", 3}, {"d", 6}});
+
+    // get: item present
+    auto el = c.get("b");
+    BOOST_TEST(el.has_value());
+    BOOST_TEST(el.value() == CacheT::Item("b", 3));
+    testCache(2, {{"c", 5}, {"d", 6}});
+
+    // get: item not present
+    el = c.get("x");
+    BOOST_TEST(!el.has_value());
+    testCache(2, {{"c", 5}, {"d", 6}});
+
+    // find: found
+    auto it1 = c.find("d");
+    BOOST_TEST(it1 != c.list().end());
+    BOOST_TEST(*it1 == CacheT::Item("d", 6));
+
+    // find: const: found
+    auto it2 = static_cast<const CacheT*>(&c)->find("c");
+    BOOST_TEST(it2 != c.list().end());
+    BOOST_TEST(*it2 == CacheT::Item("c", 5));
+
+    // find: not found
+    BOOST_TEST(c.find("x") == c.list().end());
+
+    // get: make empty
+    c.get("c");
+    c.get("d");
+    testCache(0, {});
+
+
+    // zero capacity
+    CacheT c2;
+    BOOST_TEST((c2.put("a", 1) == c2.list().end()));
+    testCache(0, {});
 }
