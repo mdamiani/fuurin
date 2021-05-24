dfb8f9bfe7b049bfe4ef657e6c7cefa4099e88b8
Mirko Damiani <mirko.damiani@outlook.com>
2021-05-24 12:15:42 +0200

    grpc: support compilation with sanitizers

--- cmake/Sanitizer.cmake
+++ cmake/Sanitizer.cmake
@@ -8,34 +8,79 @@
  # file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ##
 
+
+# Save previous contents
+
 option(ENABLE_ASAN "Build with address sanitizer" OFF)
 if(ENABLE_ASAN)
     message(STATUS "Instrumenting with Address Sanitizer")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address -fsanitize-address-use-after-scope -fno-omit-frame-pointer")
-    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fsanitize=address -fsanitize-address-use-after-scope")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address -fsanitize-address-use-after-scope")
+    set(ASAN_COMPILE_FLAGS
+        -fsanitize=address
+        -fsanitize-address-use-after-scope
+        -fno-omit-frame-pointer
+    )
+    set(ASAN_LINK_FLAGS
+        -fsanitize=address
+        -fsanitize-address-use-after-scope
+    )
 endif()
+
 
 option(ENABLE_TSAN "Build with thread sanitizer" OFF)
 if(ENABLE_TSAN)
     message(STATUS "Instrumenting with Thread Sanitizer")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=thread -g")
-    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fsanitize=thread")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=thread")
+    set(TSAN_COMPILE_FLAGS
+        -fsanitize=thread
+        -g
+    )
+    set(TSAN_LINK_FLAGS
+        -fsanitize=thread
+    )
 endif()
+
 
 option(ENABLE_MSAN "Build with memory sanitizer" OFF)
 if(ENABLE_MSAN)
     message(STATUS "Instrumenting with Memory Sanitizer")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=memory -fsanitize-memory-track-origins -g -fPIE -fno-omit-frame-pointer -fno-optimize-sibling-calls")
-    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fsanitize=memory -fPIE -pie")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=memory -fPIE -pie")
+    set(MSAN_COMPILE_FLAGS
+        -fsanitize=memory
+        -fsanitize-memory-track-origins
+        -g
+        -fPIE
+        -fno-omit-frame-pointer
+        -fno-optimize-sibling-calls
+    )
+    set(MSAN_LINK_FLAGS
+        -fsanitize=memory
+        -fPIE
+        -pie
+    )
 endif()
+
 
 option(ENABLE_UBSAN "Build with undefined behavior sanitizer" OFF)
 if(ENABLE_UBSAN)
     message(STATUS "Instrumenting with Undefined Behaviour Sanitizer")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=undefined")
-    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fsanitize=undefined")
-    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=undefined")
+    set(UBSAN_COMPILE_FLAGS
+        -fsanitize=undefined
+    )
+    set(UBSAN_LINK_FLAGS
+        -fsanitize=undefined
+    )
 endif()
+
+
+set(SANITIZERS_COMPILE_FLAGS
+    ${ASAN_COMPILE_FLAGS}
+    ${TSAN_COMPILE_FLAGS}
+    ${MSAN_COMPILE_FLAGS}
+    ${UBSAN_COMPILE_FLAGS}
+)
+
+
+set(SANITIZERS_LINK_FLAGS
+    ${ASAN_LINK_FLAGS}
+    ${TSAN_LINK_FLAGS}
+    ${MSAN_LINK_FLAGS}
+    ${UBSAN_LINK_FLAGS}
+)
